973a1c674c2d5aa0e1f6d759de8b1afa0eb53856
beautiflow <1114ny@naver.com>
2022-12-26 09:23:40 +0900

    20221225

---
+++ src/DoitAlgorithm/Practice/chap02/Q1.java
@@ -0,0 +1,32 @@
+package DoitAlgorithm.Practice.chap02;
+
+import java.util.Random;
+
+public class Q1 {
+
+    static int maxOf(int[] a){
+        int max = a[0];
+        for(int i = 0; i < a.length ; i++){
+            if(a[i] > max){
+                max = a[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+
+        System.out.println("키의 최댓값을 구합니다.");
+        int num = 1 + rand.nextInt(20);
+        int[] height = new int[num];
+
+        System.out.println("사람 수는 " + num + "입니다.");
+        System.out.println("키는 다음과 같습니다.");
+        for(int i = 0; i < num; i++){
+            height[i] = 100 + rand.nextInt(90);
+            System.out.println("height[" + i + "] :" + height[i]);
+        }
+        System.out.println("최댓값은 " + maxOf(height) + "입니다.");
+    }
+}
